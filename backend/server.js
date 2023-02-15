b1c6278bddfa21d999ecc25a2d15ac44c839e9a2
Mubleem <102492190+Mublin@users.noreply.github.com>
2023-02-15 10:22:20 +0100

    uploading update

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ app.use((err, req, res, next)=>{
 
 
 
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 10000
 app.listen(port, ()=> {
     console.log(`running at port ${port}`)
 })
